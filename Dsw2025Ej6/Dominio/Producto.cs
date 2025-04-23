056c3ea3cc21017f7cfbf8d71a38f137223af9d0
DylanSDev <dylansdiaz.dev@gmail.com>
2025-04-23 01:24:27 -0300

    Inicializamos el proyecto y agregamos la clase producto con sus atributos y metodos.

---
+++ Dsw2025Ej6/Dominio/Producto.cs
@@ -0,0 +1,93 @@
+﻿namespace Dsw2025Ej6.Dominio;
+
+internal class Producto
+{
+    private long _codigo;
+    private string _descripcion;
+    private bool _activo;
+    private double _precioVenta;
+    private float _impuesto = 0.21f;
+    private int _stock;
+    private char _presentacion;
+    private DateTime _fechaAlta;
+
+    public long GetCodigo()
+    {
+        return _codigo;
+    }
+
+    public void SetCodigo(long codigo)
+    {
+        _codigo = codigo;
+    }
+
+    public string GetDescripcion()
+    {
+        return _descripcion;
+    }
+
+    public void SetDescripcion(string descripcion)
+    {
+        _descripcion = descripcion;
+    }
+
+    public bool GetActivo()
+    {
+        return _activo;
+    }
+
+    public void SetActivo(bool activo)
+    {
+        _activo = activo;
+    }
+
+    public double GetPrecioVenta()
+    {
+        return _precioVenta;
+    }
+
+    public void SetPrecioVenta(double precioVenta)
+    {
+        _precioVenta = precioVenta;
+    }
+
+    public float GetImpuesto()
+    {
+        return _impuesto;
+    }
+
+    public void SetImpuesto(float impuesto)
+    {
+        _impuesto = impuesto;
+    }
+
+    public int GetStock()
+    {
+        return _stock;
+    }
+
+    public void SetStock(int stock)
+    {
+        _stock = stock;
+    }
+
+    public char GetPresentacion()
+    {
+        return _presentacion;
+    }
+
+    public void SetPresentacion(char presentacion)
+    {
+        _presentacion = presentacion;
+    }
+
+    public DateTime GetFechaAlta()
+    {
+        return _fechaAlta;
+    }
+
+    public void SetFechaAlta(DateTime fechaAlta)
+    {
+        _fechaAlta = fechaAlta;
+    }
+}
---
+++ Dsw2025Ej6/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Dsw2025Ej6;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
